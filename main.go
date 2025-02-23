44cbc2010235e49d222ece27ef6a898123147bba
daniilstudenikin <d.studenikin@action.tech>
2025-02-23 15:27:14 +0300

    Format user-agent string for improved readability

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 	"time"
 )
 
-const ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+const ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, " +
+	"like Gecko) Chrome/133.0.0.0 Safari/537.36"
 
 func main() {
 	client := &http.Client{}
